1e38efbc163fb34f0aef65ae98e151b6a6f9653b
aaronstarstaff <159981647+aaronstarstaff@users.noreply.github.com>
2025-04-20 15:59:44 -0700

    Update vendorsetup.sh AGAIN

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,10 +1,10 @@
-#
+0#
 # Copyright (C) 2025 The Android Open Source Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 COMMON_LUNCH_CHOICES := \
-omni_boot-user \
-omni_boot-userdebug \
-omni_boot-eng
+    omni_boot-user \
+    omni_boot-userdebug \
+    omni_boot-eng
 
